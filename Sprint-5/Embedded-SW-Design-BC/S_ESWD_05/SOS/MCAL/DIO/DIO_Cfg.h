befff6fc7b756721c88d45262b11cc1cd62f5a74
MohamedSamy <mohamedsamyn14@gmail.com>
2021-09-03 16:42:44 +0200

    Removed extra files

--- Sprint-5/Embedded-SW-Design-BC/S_ESWD_05/SOS/MCAL/DIO/DIO_Cfg.h
+++
@@ -1,6 +0,0 @@
-#ifndef DIO_CFG_H_
-#define DIO_CFG_H_
-
-#define DIO_GROUPS_NO										(4u)
-
-#endif /* DIO_CFG_H_ */
--- Sprint-5/Embedded-SW-Design-BC/S_ESWD_05/SOS/MCAL/Timer/TM_Cfg.h
+++
@@ -1,7 +0,0 @@
-#ifndef TM_CFG_H_
-#define TM_CFG_H_
-
-#define TM_GROUPS_NO								1u
-#define TM_EXTERNAL_PRESCALER_VALUE			1u 
-
-#endif /* TM_CFG_H_ */
